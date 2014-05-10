f16b39965feaea64bd4acc4788150a929d44596c
Caio Carrara <caiocarrara@gmail.com>
2014-05-10 13:56:05 -0300

    Django initial project structure and basic requirements

---
+++ custodian/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for custodian project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "custodian.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
